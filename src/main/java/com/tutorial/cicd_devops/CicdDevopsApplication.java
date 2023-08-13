bdf7a9a7b65b388c7069f68a6430f9612ea93094
FOX <fox@hotmail.com>
2023-08-14 00:32:33 +0200

    init repo

---
+++ src/main/java/com/tutorial/cicd_devops/CicdDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.cicd_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutorial/cicd_devops/CicdDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.cicd_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
